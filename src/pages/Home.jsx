68245fa3ebc87ed87ca5c917428b0c9ce6498e23
sudhiraYadav <sudhirayadav4@gmail.com>
2024-04-18 12:49:18 +0545

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,46 @@
-import React , { useEffect ,useRef} from "react";
-import Chart from "chart.js/auto";  
+import React from "react";
+// import Chart from "chart.js/auto";
 
 const Home = () => {
-  const chartRef = useRef(null);
+  // const chartRef = useRef(null);
 
-  useEffect(() => {
-    const ctx = document.getElementById("myChart").getContext("2d");
+  // useEffect(() => {
+  // const ctx = document.getElementById("myChart").getContext("2d");
 
-    // Check if a chart instance already exists
-    if (chartRef.current !== null) {
-      chartRef.current.destroy();
-    }
+  // Check if a chart instance already exists
+  // if (chartRef.current !== null) {
+  //   chartRef.current.destroy();
+  // }
 
-    // Create a new chart instance
-    chartRef.current = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-          label: "Revenue",
-          backgroundColor: "rgba(54, 162, 235, 0.5)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1,
-          data: [65, 59, 80, 81, 56, 55, 40],
-        }],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
+  // Create a new chart instance
+  // chartRef.current = new Chart(ctx, {
+  //   type: "bar",
+  //   data: {
+  //     labels: ["January", "February", "March", "April", "May", "June", "July"],
+  //     datasets: [{
+  //       label: "Revenue",
+  //       backgroundColor: "rgba(54, 162, 235, 0.5)",
+  //       borderColor: "rgba(54, 162, 235, 1)",
+  //       borderWidth: 1,
+  //       data: [65, 59, 80, 81, 56, 55, 40],
+  //     }],
+  //   },
+  //   options: {
+  //     scales: {
+  //       y: {
+  //         beginAtZero: true,
+  //       },
+  //     },
+  //   },
+  // });
 
-    // Cleanup function to destroy chart when component 
-    return () => {
-      if (chartRef.current !== null) {
-        chartRef.current.destroy();
-      }
-    };
-  }, []);
+  // Cleanup function to destroy chart when component
+  //   return () => {
+  //     if (chartRef.current !== null) {
+  //       chartRef.current.destroy();
+  //     }
+  //   };
+  // }, []);
   return (
     <>
       <div className="container mx-auto ">
@@ -81,10 +81,8 @@ const Home = () => {
             </div>
           </div>
 
-        
-
-           {/* Graph */}
-           <div className="col-md-12 mt-4">
+          {/* Graph */}
+          {/* <div className="col-md-12 mt-4">
             <div className="card shadow">
               <div className="card-body py-1">
                 <h3 className="card-title text-center fw-semibold">
@@ -93,6 +91,32 @@ const Home = () => {
                 <div className="text-center">
                 <canvas id="myChart" width="400" height="130"></canvas>
 
+                </div>
+              </div>
+            </div>
+          </div> */}
+          <div className="container py-4 mx-auto">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="capitalize fw-semibold text-primary py-4 border-bottom border-2 ">
+                  Welcome To Our RJ_DECOR
+                </h3>
+                <div className="">
+                <div className="adminCard">
+                  <strong>Admin</strong>
+                  <br />
+                  Sudhira Yadav
+                  <br />
+                  9807347863
+                  <br />
+                  Gothatar, Bhaktpur
+                </div>
+                </div>
+                
+              </div>
+              <div className="col-md-6">
+                <div className="sideimg">
+                  <img src="/FB_IMG_1712640094410.jpg" alt="" className="" />
                 </div>
               </div>
             </div>
